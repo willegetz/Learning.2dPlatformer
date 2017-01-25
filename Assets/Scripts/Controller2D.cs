ac947a2aa5f8f88c0d19e38988b85ef7aa06be06
William Getz <willegetz@gmail.com>
2017-01-25 13:30:53 -0800

    WIP Episode 3. Basic slope detection and movement.

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -12,6 +12,7 @@ public class Controller2D : MonoBehaviour
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
 
+    float maxClimbAngle = 80;
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
@@ -103,6 +104,12 @@ public class Controller2D : MonoBehaviour
 
             if (hit)
             {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (i == 0 && i <= maxClimbAngle)
+                {
+                    ClimbSlope(ref velocity, slopeAngle);
+                }
+
                 velocity.x = (hit.distance - skinWidth) * directionX;
                 rayLength = hit.distance;
 
@@ -136,4 +143,10 @@ public class Controller2D : MonoBehaviour
         }
     }
 
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+    {
+        float moveDistance = Mathf.Abs(velocity.x);
+        velocity.y = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+    }
 }
